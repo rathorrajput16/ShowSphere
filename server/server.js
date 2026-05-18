e5bd28be08768210fc2403e54c2422ed6b2b401b
Ayush <rathorrajput167@gmail.com>
2026-05-18 10:24:53 +0530

    backend setup done and inngest setup and basic functions created

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { serve } from "inngest/express";
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { functions, inngest } from './inngest/index.js';
+const app = express();
+const PORT = process.env.PORT || 5000;
+await connectDB();
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware())
+
+app.get('/',(req,res)=>res.send('server is running'));
+app.use('/api/inngest',serve({client:inngest,functions}))
+
+
+
+app.listen(PORT,()=>console.log(`Server running at port ${PORT}`));
+
