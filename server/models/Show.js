cdfdce1de386b8d686354742c604765d680c3a24
Ayush <rathorrajput167@gmail.com>
2026-05-18 10:42:41 +0530

    Show model done

---
+++ server/models/Show.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const showSchema=new mongoose.Schema({
+    movie:{type:String,required:true},
+    showDateTime:{type:String,required:true},
+    showPrice:{type:Number,required:true},
+    occupiedSeats:{type:Object,default:{}}
+})
+const Show=mongoose.model("Show",showSchema);
+export default Show;
