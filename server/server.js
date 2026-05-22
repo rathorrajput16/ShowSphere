37986ce2eee21419fe2064e9dcded419f28606dc
Ayush <rathorrajput167@gmail.com>
2026-05-22 22:40:14 +0530

    created controllers for getting movies and add shows

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import { serve } from "inngest/express";
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { functions, inngest } from './inngest/index.js';
+import showRouter from './routes/showroutes.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 await connectDB();
@@ -14,7 +15,7 @@ app.use(clerkMiddleware())
 
 app.get('/',(req,res)=>res.send('server is running'));
 app.use('/api/inngest',serve({client:inngest,functions}))
-
+app.use('/api/show',showRouter);
 
 
 app.listen(PORT,()=>console.log(`Server running at port ${PORT}`));
