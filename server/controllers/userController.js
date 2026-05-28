f2c6d0516705d489d158f37390ca74bf2089ffed
Ayush <rathorrajput167@gmail.com>
2026-05-28 14:37:42 +0530

    created create booking controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,17 @@
+import Booking from "../models/Booking.js";
+export const createBooking=async(req,res)=>{
+    try{
+        const user=req.auth().userId;
+        const bookings=await Booking.find({user}).populate({
+            path:"show",
+            populate:{path:"movie"}
+        }).sort({createdAt:-1})
+        
+        res.json({success:true,bookings})
+
+    }
+    catch(error){
+        console.error(error);
+        res.json({success:false,message:error.message})
+    }
+}
