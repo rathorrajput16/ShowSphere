a1e7a16b6474b6c66c9a291f3106a3828441fa17
Ayush <rathorrajput167@gmail.com>
2026-05-28 14:00:00 +0530

    implemented booking router

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import { clerkMiddleware } from '@clerk/express'
 import { functions, inngest } from './inngest/index.js';
 import showRouter from './routes/showroutes.js';
 import { addShow } from './controllers/showController.js';
+import bookingRouter from './routes/boookingRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 await connectDB();
@@ -17,5 +18,7 @@ app.use(clerkMiddleware())
 app.get('/',(req,res)=>res.send('server is running'));
 app.use('/api/inngest',serve({client:inngest,functions}))
 app.use('/api/show',showRouter);
+app.use('/api/booking',bookingRouter);
+
 app.listen(PORT,()=>console.log(`Server running at port ${PORT}`));
 
