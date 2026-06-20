f6118297ddcfce82f3e05dca54f2bfead36f13f2
Ayush <rathorrajput167@gmail.com>
2026-06-20 06:10:18 +0530

    minor fix

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -37,11 +37,7 @@ const datePart = showDate.toLocaleDateString();
 
   const seatsPerRow = 9
 
-  const dummyBookedSeats = {
-    '1-2026-05-16-10:15 AM': ['A2', 'A5', 'B3', 'D4', 'F8'],
-    '1-2026-05-16-7:15 PM':  ['A1', 'A6', 'B7', 'D3', 'D4', 'H8', 'I5'],
-    '2-2026-05-17-1:00 PM':  ['C4', 'C5', 'E7', 'J2'],
-  }
+
 
   const showKey = `${movieId}-${date}-${time}`
 const getOccupiedSeats = async () => {
--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -115,10 +115,7 @@ export const addShow = async (req, res) =>{
         }
 
               
-         await inngest.send({
-            name: "app/show.added",
-             data: {movieTitle: movie.title}
-         })
+        
 
         res.json({success: true, message: 'Show Added successfully.'})
     } catch (error) {
